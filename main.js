9e6467d3fd48e171229a0a4a9a3a9c29fcae414d
leticode <>
2024-12-07 20:20:43 -0300

    primer commit del proyecto del juego del ahorcado

---
+++ main.js
@@ -0,0 +1,13 @@
+let contenedorPrincipal = document.querySelector(".contenedorPrincipal");
+
+let letras = ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for (let letra of letras){
+    let contenedorTeclado = document.createElement("div");
+    contenedorTeclado.className = "contenedorTeclado";
+    contenedorPrincipal.appendChild(contenedorTeclado);
+    let teclado = document.createElement("button");
+    teclado.className = "btnTeclado";
+    contenedorTeclado.appendChild(teclado);
+}
+
